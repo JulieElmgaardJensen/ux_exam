2cf995872bf454782b47cff5257999cc9d328031
chmo <chmo0001@stud.kea.dk>
2023-11-19 13:09:48 +0100

    hjælp mig. login virker ikke uden trim, men har tjekket input og det matcher i databasen. så tænker jeg, om det er fordi, min connection til databasen ikke er rigtig, meeeen så ved jeg ikke, hvad jeg gør forkert - hjæææælp

---
+++ js/login.js
@@ -0,0 +1,52 @@
+// JAVASCRIPT STRICT MODE
+'use strict';
+
+// GET LOGIN FORM AND RUN ASYNC FUNCTION WHEN SUBMITTED
+document.getElementById('frm_login').addEventListener('submit', async function(event) {
+    // PREVENTING DEFAULT TO RUN ASYNC FUNCTION (TROR DEN LADER OS KØRER JAVASCRIPT FØR DEN KØRER FRONTEND VALIDATION, SKAL LIGE TJEKKE OP PÅ DET)
+    event.preventDefault();
+
+    // GETTING EMAIL AND PASSWORD INPUT FROM FORM AND DECLARING THEM IN CONSTS
+    const email_input = document.getElementById('email');
+    const password_input = document.getElementById('password');
+
+    // // TRIMMING EMAIL AND PASSWORD INPUT (KAN IKKE FÅ INPUT TIL AT VÆRE VALID MED MINDRE DEN TRIMMER???)
+    const email = email_input.value.trim();
+    const password = password_input.value.trim();
+
+   // LOGGING EMAIL AND PASSWORD
+   console.log(email_input.value, password_input.value);
+
+    try {
+        // ASYNC HTTP REQUEST FOR JSON SERVER
+        const response = await fetch('http://localhost:3000/users');
+        // WAITING FOR DATA FETCH
+        const users = await response.json();
+    
+        console.log('Fetched user data:', users);
+    
+        // FIND() METHOD RETURNS FIRST ELEMENT THAT MATCHES THE EMAIL
+        const user = users.find(u => u.email === email);
+    
+        // IF THE USER EMAIL VALUE IS ASSIGNED IN THE JSON DATABASE AND THE INPUT PASSWORD MATCHES A PASSWORD IN THE DATABASE THE USER WILL BE REDIRECTED TO THE SHOP
+        // ELSE THE USER WILL SEE AN ALERT SAYING WRONG USER CREDENTIALS
+        if (user) {
+            if (user.password === password) {
+                console.log('Login successful!');
+                // SAVING EMAIL IN THE SESSION
+                sessionStorage.setItem('user_email', email);
+                // REDIRECT TO THE SHOP IF USER LOGIN IS VALID AND EXISTS IN JSON DATABASE
+                window.location.href = 'shop.html';
+            } else {
+                console.log('Incorrect password');
+                alert('Incorrect password');
+            }
+        } else {
+            console.log('Email does not exist in the database');
+            alert('Invalid email');
+        }
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+   
+});

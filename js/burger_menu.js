beb57e782166b451019441d39d2d659237704210
maja.norgaard@gmail.com <maja>
2023-11-19 11:57:39 +0100

    added js file, margin top and header style

---
+++ js/burger_menu.js
@@ -0,0 +1,13 @@
+function burger_menu_dropdown() {
+    // Find the div from ID
+    var dropdown_menu = document.getElementById("dropdown_menu");
+
+    // If the style of the div is block hide it
+    if (dropdown_menu.style.display === "block") {
+      dropdown_menu.style.display = "none";
+    } 
+    // If the style of the div isn't block show it
+    else {
+      dropdown_menu.style.display = "block";
+    }
+  }

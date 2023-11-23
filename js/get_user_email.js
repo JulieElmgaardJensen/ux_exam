9f6a74d4f2efe1b36134e5670f7b7cb5c43e9adb
chmo <chmo0001@stud.kea.dk>
2023-11-23 14:30:42 +0100

    changed css login and signup and redirecting the user if they're not logged in, but it's commented out so it doesn't interfere with our work

--- js/get_user_email.js
+++ js/get_user_email.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Retrieve email from sessionStorage
     const user_email = sessionStorage.getItem('user_email');
 
+    // COMMENTED OUT SO WE DON'T HAVE TO BE LOGGED IN WHEN WE TEST
     // // Check if the user is logged in or not
     // if (!user_email) {
     //     // If email is not set, redirect to login page
-    //     window.location.href = 'login.html';
+    //     window.location.href = 'index.html';
     // } else {
         // Else continue loading content and inserting email into a paragraph with id 'userEmailParagraph'
         const user_email_paragraph = document.getElementById('user_logged_in');
